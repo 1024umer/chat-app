d7d6c1134f440ba7a7fb09158f704fa10a22ca6c
1024umer <127780703+1024umer@users.noreply.github.com>
2024-02-01 01:40:44 +0500

    home jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,51 +1,51 @@
 import React, { useEffect } from 'react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
-import { Container,Box,Text,Tab,TabList,TabPanel,TabPanels,Tabs } from '@chakra-ui/react'
+import { Container, Box, Text, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import Login from '../components/Login'
 import SignUp from '../components/SignUp'
 import { useNavigate } from 'react-router-dom'
 
 function Home() {
   const navigate = useNavigate();
-  useEffect(()=>{
-      const userInfo = localStorage.getItem('userInfo');
-      if(userInfo){
-          navigate('/chats');
-      }
-  },[navigate])
+  useEffect(() => {
+    const userInfo = localStorage.getItem('userInfo');
+    if (userInfo) {
+      navigate('/chats');
+    }
+  }, [navigate])
   return (
     <div>
       <Container maxW='xl' centerContent >
         <Box
-             text='center'
-            d="flex"
-            justifyContent="center"
-            p={3}
-            bg="white"
-            w="100%"
-            m="40px 0 15px 0"
-            borderRadius="lg"
-            borderWidth="1px"
-            >
-            <Text fontSize="4xl" fontFamily="Work sans">
-                Talk-A-Tive
-            </Text>
+          text='center'
+          d="flex"
+          justifyContent="center"
+          p={3}
+          bg="white"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Text fontSize="4xl" fontFamily="Work sans">
+            Talk-A-Tive
+          </Text>
         </Box>
         <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-            <Tabs isFitted variant="soft-rounded">
+          <Tabs isFitted variant="soft-rounded">
             <TabList mb="1em">
-                <Tab>Login</Tab>
-                <Tab>Sign Up</Tab>
+              <Tab>Login</Tab>
+              <Tab>Sign Up</Tab>
             </TabList>
             <TabPanels>
-                <TabPanel>
+              <TabPanel>
                 <Login />
-                </TabPanel>
-                <TabPanel>
+              </TabPanel>
+              <TabPanel>
                 <SignUp />
-                </TabPanel>
+              </TabPanel>
             </TabPanels>
-            </Tabs>
+          </Tabs>
         </Box>
       </Container>
     </div>
